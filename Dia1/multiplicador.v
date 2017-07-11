a0b12b806b08f40e4dc8f6d649ebaba14dd3ad1d
Unknown <scheunemanngame@gmail.com>
2017-07-11 08:33:18 -0300

    1

--- Dia1/multiplicador.v
+++ Dia1/multiplicador.v
@@ -10,7 +10,7 @@
 
 module multiplicador(clk,A,B,S); //Definicao das ports
 	parameter TAM=8;
-	input [TAM-1:0] A;	//sinal de entrada A
+	input [TAM-1:0] A	//sinal de entrada A
 	input [TAM-1:0] B; 	//sinal de entrada B
 	input clk;			//sinal de clock
 	output reg [(TAM+TAM-1):0] S; // sinal de saida
